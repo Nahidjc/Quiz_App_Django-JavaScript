6e543a8e83ee069e6cdcaf7225f34f90072db256
Nahid2194 <nahidjc80@gmail.com>
2021-01-31 11:27:05 +0600

    frontend setup

--- quizApp/views.py
+++ quizApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Exam
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html', context={})
+    exams = Exam.objects.all()
+    return render(request, 'index.html', context={'exams': exams})
